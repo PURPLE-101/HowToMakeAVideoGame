d40d017d0721830f5d2e8418e13ca748967253e0
Andrew <khenkinandrew@gmail.com>
2021-01-03 12:38:10 -0500

    Player can now move left and right. Also added a physics material

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,10 +4,23 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public Rigidbody rb;
-    
+
+    public float forwardForce = 2000f;
+    public float sideWayForce = 500f;
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000*Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce*Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sideWayForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideWayForce * Time.deltaTime, 0, 0);
+        }
     }
 }
