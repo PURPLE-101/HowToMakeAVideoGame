bc133472c738aaa7e89b33c6df7e4dd13514cd49
Andrew <khenkinandrew@gmail.com>
2021-01-03 14:28:43 -0500

    Added more obstacles and did some small changes to player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,12 +15,12 @@ public class PlayerMovement : MonoBehaviour
 
         if(Input.GetKey("d"))
         {
-            rb.AddForce(sideWayForce * Time.deltaTime, 0, 0);
+            rb.AddForce(sideWayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-sideWayForce * Time.deltaTime, 0, 0);
+            rb.AddForce(-sideWayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
     }
 }
